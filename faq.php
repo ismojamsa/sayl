29ca3ac02ee4b2e1a8ce9e74a41328de677dfadc
ismo.jamsa <ismo.jamsa@student.kpedu.fi>
2017-01-31 12:58:41 +0200

    Style footer päivitetty

--- faq.php
+++ faq.php
@@ -9,16 +9,14 @@
         <link rel="stylesheet" href="http://cosmo.kpedu.fi/~ismojamsa/php2017/git/sayl/tyyli.css">
     </head>
     <body>
-    
-    
-    
-    
-    
-    
+      <div class="container-fluid text-center">
+      </div>
+      
+      
         <footer class="footer">
-           <div class="container">
-             <div class="row">
-               <div class="col-sm-12 vali3">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-12">
                 <p>Eteläranta 6 A 8, 3. krs, 00130 Helsinki, Finland. sayl@sayl.fi Tekninen toteutus by Joan</p>
               </div>
             </div>
