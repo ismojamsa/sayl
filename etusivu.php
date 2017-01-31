6a02b15ed7e3bbcd7cb34a72b5ad2f8b49981076
arttu.uuskartano <arthur.newmansion@gmail.com>
2017-01-31 12:45:45 +0200

    etusivu päivitetty

--- etusivu.php
+++ etusivu.php
@@ -6,9 +6,26 @@
         <meta name="viewport" content="width-device-width,initial-scale=1.0"/>  
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation/6.3.0/foundation.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation-icons/3.0/foundation-icons.min.css">
+        <link rel="stylesheet" href="http://cosmo.kpedu.fi/~arttuuuskartano/php2017/SAYL/sayl/tyyli.css">
     </head>
     <body>
     
+     <header>
+      <h1 style="color:white;">Suomi-Amerikka yhdistyneiden liitto</h1>
+      <h5 style="color:white;">SAYL</h5>
+     </header>          
+     
+     <nav>
+       <ul>
+        <li><a href="">Etusivu</a>
+        <li><a href="">Jäsennyys</a>
+        <li><a href="">Aktiviteetit</a>
+        <li><a href="">Matkailu</a>
+        <li><a href="">Julkaisut</a>
+        <li><a href="">FAQ</a>
+        <li><a href="">Yhteystiedot</a>       
+       </ul>
+     </nav>
     
 
 
