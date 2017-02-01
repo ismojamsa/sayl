377146e3c44f85ebb448149d8dc74b3064502667
ismo.jamsa <ismo.jamsa@student.kpedu.fi>
2017-02-01 10:15:36 +0200

    faq päivitetty

--- faq.php
+++ faq.php
@@ -37,11 +37,52 @@
          <li><a href="">Yhteystiedot</a></li>
        </ul>
      </nav>
-    
-    
-    
-              
-      
+     
+     <p></p>
+     
+       <div class="row">
+         <div class="small-12 medium-8 columns">
+           <h4>1.  Mitä hyötyä minulle olisi SAYL:n jäsenyydestä?</h4>
+           <p>Ystävyysseuran jäsenenä saat monipuolista tietoa Yhdysvalloista. 
+           Kanavinamme ovat: Suomi-USA –jäsenlehti neljästi vuodessa, sähköinen USA-uutiskirje, 
+           noin 200 eri puolilla Suomea järjestettävää Amerikka-tilaisuutta vuodessa ja 
+           aktiivinen Facebook-ryhmä. Olet samalla tekemässä suomesta kansainvälisillä 
+           markkinoilla pärjäävää maata. Lähetämme kymmeniä nuoria opiskeluvaihtoon ja 
+           työharjoitteluun Yhdysvaltoihin. SAYL:n apurahasäätiöstä voit anoa apurahoja 
+           jatko-opiskeluun Yhdysvalloissa. Summer Work and Travel –ohjelmamme tarjoaa 
+           suomalaisnuorille kesätöitä Wisconsinissa. Englanninkieliset leikkikoulut ja 
+           päiväkodit toimivat paikallisyhdistyksissä muutamilla paikkakunnilla. 
+           Tule mukaan - verkostoidu, solmi uusia ystävyyksiä ja opi lisää Amerikasta.</p>
+           <h4>2.  Olen joskus vuosia sitten ollut SAYL:n jäsen. Silloin matkustin edullisesti Amerikkaan. Vieläkö tämä jäsenetu on
+           voimassa?</h4>
+           <p>Ei. Suomen valmismatkalaki muuttui vuonna 1995 ja sen myötä nämä halvat lennot loppuivat. 
+           SAYL:lla ei voittoa tuottamattomana ystävyysseurana ole mahdollisuuksia tallettaa Kuluttajavirastoon 
+           nykyään vaadittavia takuita kaupallisten matkajärjestäjien lailla. Toisaalta hintakilpailu Atlantin 
+           ylittävillä lennoilla on niin kova, että harva ostaisi lentolippuja entisen malliin, jopa vuotta 
+           ennen reissua. Internet-kauppa on myös osaltaan muuttanut ihmisten matkailutottumuksia.
+           Meillä on lentoyhtiö- ja matkatoimistokumppaneita, joiden kanssa järjestämme mm. erilaisia teemamatkoja 
+           Yhdysvaltoihin. Toisinaan yhteistyö näkyy myös tarjouksina jäsekunnalle.</p>
+         </div>
+         <div class="small-12 medium-4 columns">
+           <img src="http://sayl.fi/wp-content/uploads/2013/04/faq.jpg"width="100%" height="">
+         </div>
+       </div>
+       <div class="row">
+         </div class="small-12 columns">
+           <h4>3.  Saako SAYL taloudellista tukea amerikkalaisilta?</h4>
+           <p>Ei. SAYL:n rahoitus koostuu jäsenmaksuista, Suomen opetus- ja kulttuuriministeriön myöntämästä 
+           ystävyysseuratoimintaan suunnatusta valtionavustuksesta ja mahdollisista projektikohtaisista avustuksista. 
+           Yhdysvaltain liittovaltiolta emme saa säännöllistä taloudellista tukea, mutta yhteistyömme Yhdysvaltain 
+           Suomen-suurlähetystön eri osastojen kanssa kanssa on hyvin aktiivista ja läheistä. </p>
+           <h4>4.  Mistä saan tietoa kielikokeista ja tasotesteistä?</h4>
+           <p>Tietoa Suomessa järjestettävistä SAT-testistä ja muista kansainvälistä kielitesteistä löydät suomeksi 
+           näiltä sivuilta kohdasta http://sayl.fi/kielitestit/Muista lukea ilmoittautumisohjeet huolella ja olla 
+           testipaikalla hyvissä ajoin.</p>
+           <p>Yhdysvaltain Suomen-suurlähetystön sivuilta löytyy myös usein kysyttyjä kysymyksiä ja vastauksia niihin:
+           http://finland.usembassy.gov/fi/faqs.html</p>
+         </div>
+       </div>
+           
       </div>
         <footer class="footer">
           <div class="container reunat">
